34f662d6d243c5ee403cc30fdc0dc7ff3e00e38a
Danz29 <99729025+Danz29@users.noreply.github.com>
2024-08-28 12:37:13 +0700

    Add files via upload

---
+++ javaapplication1/Mahasiswa.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Fardan
+ */
+public class Mahasiswa {
+     // Metode membaca
+    public void membaca() {
+        System.out.println("Mahasiswa sedang membaca buku.");
+    }
+
+    // Metode nyontek
+    public void nyontek() {
+        System.out.println("Mahasiswa sedang mencoba nyontek.");
+    }
+
+    // Metode modifikasi
+    public void modifikasi() {
+        System.out.println("Mahasiswa sedang memodifikasi tugas.");
+    }
+}
---
+++ javaapplication1/MahasiswaBeraksi.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Fardan
+ */
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        // Buat objek Mahasiswa
+        Mahasiswa mahasiswa = new Mahasiswa();
+
+        // Panggil metode membaca
+        mahasiswa.membaca();
+
+        // Panggil metode nyontek
+        mahasiswa.nyontek();
+
+        // Panggil metode modifikasi
+        mahasiswa.modifikasi();
+    }
+}
